77454edf3cd293dabdb15dd21dfcb3e542e6573d
Klemen Nagode <klemen.nagode@gmail.com>
2014-10-30 11:15:57 +0100

    init

--- xxxx/ViewController.m
+++ xxxx/ViewController.m
@@ -8,8 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
 
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @end
 
 @implementation ViewController
@@ -17,11 +18,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    MKCircle *circle = [MKCircle circleWithCenterCoordinate:CLLocationCoordinate2DMake(3.970639, 12.238926) radius: 144];
+    [_mapView addOverlay:circle];
+    _mapView.delegate = self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)buttonPressedHandler:(id)sender {
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake(3.970639, 12.238926), 100, 100);
+    [self.mapView setRegion:viewRegion animated:YES];
 }
+
+- (MKOverlayView *)mapView:(MKMapView *)mapView viewForOverlay:(id)overlay
+{
+    MKCircleView* circleView = [[MKCircleView alloc] initWithOverlay:overlay] ;
+    circleView.fillColor = [UIColor redColor];
+    
+    return circleView;
+}
+
 
 @end
